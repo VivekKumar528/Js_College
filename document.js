0dfdd6b3b1209ecee12f5b25220e0aa01cdb4a22
VivekKumar528 <vicky2820x@gmail.com>
2024-08-13 12:08:22 +0530

    13/08/2024

---
+++ document.js
@@ -0,0 +1,9 @@
+// document.write('Welcome to GLA')
+// document.write('<br>')
+// document.write('<hr>')
+// document.write('<h2>Branches in GLA <h2>')
+// document.write('<h2>CSE<h2>')
+// document.write('<h2>Mechanical<h2>')
+// document.write('<h2>Elecrical<h2>')
+
+
